a3f350c15b80a0d67c06d1bcc2b6da3b01516fdf
Erik Russell <erikrussells@gmail.com>
2022-12-28 13:09:51 -0500

    commit tree 0aa36e29d42744fce5a6475083d98c60d80fd0e8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,19 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :item_not_found_error
 
   def index
     items = Item.all
     render json: items, include: :user
+  end
+
+  def create
+    itme = User.create!(params[:id])
+    render json: user, include: :user
+  end
+
+  private
+  def item_not_found_error
+    render json: {error: "item not found"}, status: :not_found
   end
 
 end
